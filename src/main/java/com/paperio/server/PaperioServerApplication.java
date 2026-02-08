2894822e2e0f72ce297021f0ef0c13d69661930f
Mateusz Kaszubowski <m.aizenvolt@gmail.com>
2026-02-08 18:43:46 +0100

    Initial commit
    (cherry picked from commit a774ce27c1e1d593b106b45127e88b4f43eca593)

---
+++ src/main/java/com/paperio/server/PaperioServerApplication.java
@@ -0,0 +1,13 @@
+package com.paperio.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperioServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperioServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paperio/server/PaperioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperio.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
